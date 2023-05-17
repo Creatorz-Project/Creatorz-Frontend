6091e601405d0b7ec500061dc0f535aa7b0ee225
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-05-17 18:35:37 +0530

    added related video

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -10,39 +10,6 @@ export default function Main(props) {
   const [videosData, setVideosData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // const forloop = useCallback(async () => {
-  //   setLoading(true);
-
-  //   const tempChoicesArray = [];
-
-  //   var requestOptions = {
-  //     method: "GET",
-  //     redirect: "follow",
-  //   };
-
-  //   for (let i = 0; i < videos.length; i++) {
-  //     let obj = {};
-  //     const newresponse = await fetch(
-  //       `https://ipfs.io/ipfs/${videos[i].URI}/RoomMetaData.json`,
-  //       requestOptions
-  //     );
-  //     const result = await newresponse.json();
-  //     obj = { ...result, ...videos[i] };
-  //     tempChoicesArray.push(obj);
-
-  //     setLoading(false);
-  //   }
-  //   setVideosData(tempChoicesArray);
-  // }, [videos, videosData]);
-
-  // useEffect(() => {
-  //   if (videos.length > 0) {
-  //     forloop();
-  //   }
-  // }, [videos]);
-
-  // console.log(videos, videosData);
-
   return (
     <div className="w-full flex flex-row">
       <Backdrop
@@ -51,7 +18,7 @@ export default function Main(props) {
       >
         <CircularProgress color="inherit" />
       </Backdrop>
-      <div className="flex flex-row flex-wrap gap-5 mx-5 my-5">
+      <div className="flex flex-row flex-wrap gap-5 mx-5 my-5 justify-center">
         {videos.length > 0 ? (
           videos
             .map((data, index) => {
--- src/pages/video.js
+++ src/pages/video.js
@@ -4,106 +4,16 @@ import Video from "/src/components/Videos/Videos.js";
 import VideoComponent from "/src/components/VideoContainer";
 import { useRouter } from "next/router";
 
-export default function VideoPage() {
+export default function VideoPage(props) {
     const router = useRouter()
     const [video, setVideo] = useState(null);
-    const [relatedVideos, setRelatedVideos] = useState([]);
+    const [relatedVideos, setRelatedVideos] = useState(props.post.videos);
     const [videosData, setVideosData] = useState([])
 
     const client = useApolloClient();
     const query = router.query.id
     console.log(query)
-
-
-    const GET_VIDEOS = gql`
-        query videos(
-          $first: Int
-          $skip: Int
-          $orderBy: Video_orderBy
-          $orderDirection: OrderDirection
-          $where: Video_filter
-        ) {
-            videos(orderBy: BlockTimestamp, orderDirection: desc) {
-                id
-                VideoId
-                Owner
-                URI
-                Room
-                IsListed
-                IsPublished
-                Price
-                BlockTimestamp
-              }
-        }
-      `;
-
-    // const getRelatedVideos = () => {
-    //     client
-    //         .query({
-    //             query: GET_VIDEOS,
-    //             variables: {
-    //                 first: 20,
-    //                 skip: 0,
-    //                 where: {},
-    //             },
-    //             fetchPolicy: "network-only",
-    //         })
-    //         .then(({ data }) => {
-    //             console.log(data)
-    //             setRelatedVideos(data.videos);
-    //         })
-    //         // .catch((err) => {
-    //         //     alert("Something went wrong. please try again.!", err.message);
-    //         // });
-    // };
-
-    // useEffect(() => {
-    //     getRelatedVideos();
-    // }, [video]);
-
-    // const forloop = useCallback(async () => {
-
-
-    //     const tempChoicesArray = [];
-
-    //     var requestOptions = {
-    //         method: "GET",
-    //         redirect: "follow",
-    //     };
-
-    //     for (let i = 0; i < relatedVideos.length; i++) {
-    //         let obj = {};
-    //         const newresponse = await fetch(
-    //             `https://ipfs.io/ipfs/${relatedVideos[i].URI}/RoomMetaData.json`,
-    //             requestOptions
-    //         );
-    //         const result = await newresponse.json();
-    //         obj = { ...result, ...relatedVideos[i] };
-    //         tempChoicesArray.push(obj);
-
-    //     }
-    //     setVideosData(tempChoicesArray);
-    // }, [relatedVideos, videosData]);
-
-    // useEffect(() => {
-
-    //     if (relatedVideos.length > 0) {
-    //         forloop()
-    //     }
-
-    // }, [relatedVideos])
-
-    // useEffect(() => {
-    //     if (videosData.length > 0) {
-    //         const video = videosData.find(
-    //             (video) => video.VideoId === router.query.id
-    //         );
-    //         setVideo(video);
-    //     }
-    // }, [videosData])
-
-    // console.log(videosData)
-    // console.log(video)
+    console.log(relatedVideos)
 
     return (
         <div className="w-full bg-[#1a1c1f]  flex flex-row min-h-screen">
@@ -117,21 +27,50 @@ export default function VideoPage() {
                             <h4 className="text-md font-bold text-white ml-5 mb-3">
                                 Related Videos
                             </h4>
-                            {/* {videosData.map((video) => (
+                            {relatedVideos.map((video) => (
                                 <div
                                     onClick={() => {
-                                        setVideo(video);
+                                        // Navigation to the video screen (which we will create later)
+                                        window.location.href = `/video?id=${video.id}`;
                                     }}
                                     key={video.id}
                                 >
                                     <Video video={video} horizontal={true} />
                                 </div>
-                            ))} */}
+                            ))}
                         </div>
                     </div>
                 )}
             </div>
         </div>
     );
+}
 
+export async function getServerSideProps() {
+
+    const post = {}
+
+    console.log("getting video list")
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('x-tva-sa-id', 'srvacc_kb0ub280r8mf2wsgjrp2q31tq');
+    myHeaders.append('x-tva-sa-secret', '59iwg4ev1s99u5vh3yt2btgv71ud8vpp')
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+
+    };
+
+    const response = await fetch(`https://api.thetavideoapi.com/video/srvacc_kb0ub280r8mf2wsgjrp2q31tq/list?page=1&number=100`, requestOptions)
+    const result = await response.json()
+    console.log(result)
+    post.videos = result.body.videos
+
+    return {
+        props: {
+            post,
+        },
+    };
 }
