9ee68e7f94bc624710176c17a1abdd7ee6076fee
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2023-05-18 17:32:25 +0530

    Added Polybase

---
+++ src/utils/getUserDetails.js
@@ -0,0 +1,6 @@
+import { Polybase } from "@polybase/client";
+
+const db = new Polybase({
+  defaultNamespace:
+    "pk/0xdaf07b7db43321236f6739b10bff96379508a07d2bcbd793b4c22c31711c795d5ca356ad7fd4d8b7691aa36f7f6b44d8106538a54f41e49174aab02e64bd3cde/Creatorz",
+});
