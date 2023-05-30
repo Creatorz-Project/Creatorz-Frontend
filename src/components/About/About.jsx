d32feeea77280f00820bfa618456de8975f2dff3
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2023-05-30 22:20:50 +0530

    added Pubishing Functionality

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,51 +1,59 @@
-import ReactMarkdown from 'react-markdown'
-
 export default function About() {
-    return (
-        <>
-            {/* Creating a hero component with black background and centering everything in the screen */}
-            <section className="relative flex flex-col h-screen justify-center items-center bg-[#150A22]">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                    <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-                        <div className="text-center pb-12 md:pb-16">
-                            <h1
-                                className="text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
-                                data-aos="zoom-y-out"
-                            >
-
-                                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
-                                    Creatorz
-                                </span>
-                                {" "}empowering Creators
-                            </h1>
-                            {/* <div className="max-w-3xl mx-auto">
-                                <p
-                                    className="text-xl text-gray-400 mb-3"
-                                    data-aos="zoom-y-out"
-                                    data-aos-delay="150"
-                                >
-                                    Creatorz is a decentralized video sharing platform where content creators can monetize their videos with social tokens and NFTs. Users can buy, sell, and collect videos as NFTs and earn advertising rewards. Creatorz is a video platform that empowers creators and users with web3
-                                </p>
-                            </div> */}
-                            <ReactMarkdown className="text-xl text-gray-400 mb-8">
-                                Introducing **CREATORZ**, a revolutionary blockchain-based content-sharing platform that empowers creators to monetize their talent and reach new heights in the digital world. With CREATORZ, your creativity becomes a valuable asset, supported by cutting-edge technology and the power of blockchain.
-
-                                On CREATORZ, creators can transform their videos into unique Non-Fungible Tokens (NFTs), giving their work an exclusive digital identity and unprecedented value. By leveraging the blockchain, CREATORZ ensures the authenticity, scarcity, and traceability of these NFTs, making them highly sought-after collectibles in the digital space.
-
-                                But that's not all! CREATORZ goes beyond traditional content-sharing platforms by allowing creators to earn cryptocurrency through various revenue streams. By allowing ads to be displayed alongside their videos, creators can generate income based on viewership, providing an incentive to create high-quality and engaging content.
-
-                                Additionally, CREATORZ introduces the concept of "NFT Gated" content, enabling creators to offer exclusive access to specific videos or parts of their content in exchange for cryptocurrency. This unique feature allows creators to build a loyal fan base and provide additional value to their supporters.
-
-                                CREATORZ introduces the ability to mint their social tokens to further empower creators. These tokens represent a creator's brand and influence, providing a direct connection between the creator and their community. By minting social tokens, creators can reward their most dedicated fans and incentivize engagement within their ecosystem.
-
-                                CREATORZ doesn't stop there; it also offers a vibrant marketplace where all these tokens, including NFTs and social tokens, can be listed and traded. This marketplace connects creators, fans, and collectors, fostering a thriving ecosystem where value can be exchanged, and creativity can flourish.
-
-                                With CREATORZ, the future of content creation and monetization is here. Join us on this groundbreaking platform and unlock the full potential of your creativity while embracing the exciting world of blockchain technology. Let your talent shine, earn crypto, and connect with a passionate community of creators and enthusiasts. The possibilities are limitless with CREATORZ.
-                            </ReactMarkdown>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="relative flex flex-col h-screen justify-center items-center bg-[#150A22]">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="text-center pb-12 md:pb-16">
+              <h1
+                className="text-5xl text-white md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+                data-aos="zoom-y-out"
+              >
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                  Creatorz
+                </span>{" "}
+                empowering Creators
+              </h1>
+              Introducing **CREATORZ**, a revolutionary blockchain-based
+              content-sharing platform that empowers creators to monetize their
+              talent and reach new heights in the digital world. With CREATORZ,
+              your creativity becomes a valuable asset, supported by
+              cutting-edge technology and the power of blockchain. On CREATORZ,
+              creators can transform their videos into unique Non-Fungible
+              Tokens (NFTs), giving their work an exclusive digital identity and
+              unprecedented value. By leveraging the blockchain, CREATORZ
+              ensures the authenticity, scarcity, and traceability of these
+              NFTs, making them highly sought-after collectibles in the digital
+              space. But that's not all! CREATORZ goes beyond traditional
+              content-sharing platforms by allowing creators to earn
+              cryptocurrency through various revenue streams. By allowing ads to
+              be displayed alongside their videos, creators can generate income
+              based on viewership, providing an incentive to create high-quality
+              and engaging content. Additionally, CREATORZ introduces the
+              concept of "NFT Gated" content, enabling creators to offer
+              exclusive access to specific videos or parts of their content in
+              exchange for cryptocurrency. This unique feature allows creators
+              to build a loyal fan base and provide additional value to their
+              supporters. CREATORZ introduces the ability to mint their social
+              tokens to further empower creators. These tokens represent a
+              creator's brand and influence, providing a direct connection
+              between the creator and their community. By minting social tokens,
+              creators can reward their most dedicated fans and incentivize
+              engagement within their ecosystem. CREATORZ doesn't stop there; it
+              also offers a vibrant marketplace where all these tokens,
+              including NFTs and social tokens, can be listed and traded. This
+              marketplace connects creators, fans, and collectors, fostering a
+              thriving ecosystem where value can be exchanged, and creativity
+              can flourish. With CREATORZ, the future of content creation and
+              monetization is here. Join us on this groundbreaking platform and
+              unlock the full potential of your creativity while embracing the
+              exciting world of blockchain technology. Let your talent shine,
+              earn crypto, and connect with a passionate community of creators
+              and enthusiasts. The possibilities are limitless with CREATORZ.
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/MySpace/VideoCard.jsx
+++ src/components/MySpace/VideoCard.jsx
@@ -13,201 +13,194 @@ import { getContract } from "@/utils/Constants/Contracts";
 import { ContentManager as CMAddresss } from "../../utils/Constants/Addresses";
 import { ContentManager as CMABI } from "../../utils/Constants/ABIs";
 
-const contract = getContract(CMAddresss, CMABI);
-
 const Android12Switch = styled(Switch)(({ theme }) => ({
-    padding: 8,
-    "& .MuiSwitch-track": {
-        borderRadius: 22 / 2,
-        "&:before, &:after": {
-            content: '""',
-            position: "absolute",
-            top: "50%",
-            transform: "translateY(-50%)",
-            width: 16,
-            height: 16,
-        },
-        "&:before": {
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                theme.palette.getContrastText(theme.palette.primary.main)
-            )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
-            left: 12,
-        },
-        "&:after": {
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-                theme.palette.getContrastText(theme.palette.primary.main)
-            )}" d="M19,13H5V11H19V13Z" /></svg>')`,
-            right: 12,
-        },
+  padding: 8,
+  "& .MuiSwitch-track": {
+    borderRadius: 22 / 2,
+    "&:before, &:after": {
+      content: '""',
+      position: "absolute",
+      top: "50%",
+      transform: "translateY(-50%)",
+      width: 16,
+      height: 16,
     },
-    "& .MuiSwitch-thumb": {
-        boxShadow: "none",
-        width: 16,
-        height: 16,
-        margin: 2,
+    "&:before": {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main)
+      )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+      left: 12,
     },
+    "&:after": {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main)
+      )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+      right: 12,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "none",
+    width: 16,
+    height: 16,
+    margin: 2,
+  },
 }));
 
 export default function VideoCard(props) {
+  const [enableVideoListing, setEnableVideoListing] = useState(false);
+  const [price, setPrice] = useState(0);
+  const [open, setOpen] = useState(false);
 
-    const [enableVideoListing, setEnableVideoListing] = useState(false);
-    const [price, setPrice] = useState(0);
-    const [open, setOpen] = useState(false)
+  const openHandler = () => {
+    setOpen(!open);
+  };
 
-    const openHandler = () => {
-        setOpen(!open)
+  const ListingPrice = (event, data) => {
+    if (event.target.value > 0) {
+      setEnableVideoListing(true);
+      setPrice(event.target.value);
+      console.log(event.target.value);
     }
+  };
 
-    const ListingPrice = (event, data) => {
-        if (event.target.value > 0) {
-            setEnableVideoListing(true);
-            setPrice(event.target.value);
-            console.log(event.target.value);
-        }
-    };
+  const ListVideo = async (event, data) => {
+    console.log(data);
+    const contract = await getContract(MarketplaceAddress, MarketplaceABI);
+    if (event.target.checked == true) {
+      const tx = await contract.listVideo(data.id, price);
+      await tx.wait();
+    } else {
+      const tx = await contract.unListVideo(data.id);
+      await tx.wait();
+    }
+  };
 
-    const ListVideo = async (event, data) => {
-        console.log(data);
-        const contract = await getContract(MarketplaceAddress, MarketplaceABI);
-        if (event.target.checked == true) {
-            const tx = await contract.listVideo(data.id, price);
-            await tx.wait();
-        } else {
-            const tx = await contract.unListVideo(data.id);
-            await tx.wait();
-        }
-    };
+  // use props.data to get all data related to video
+  const PublishVideo = async (ownerPercentage, holderPercentage, enableAds) => {
+    openHandler();
+    console.log(ownerPercentage, holderPercentage, enableAds);
+    const contract = await getContract(CMAddresss, CMABI);
+    try {
+      const tx = await contract.publishVideo(
+        props.data.id,
+        ownerPercentage,
+        holderPercentage,
+        enableAds
+      );
+      await tx.wait();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const UnPublishVideo = async (event) => {
     // use props.data to get all data related to video
-    const PublishVideo = async (ownerPercentage, holderPercentage, enableAds) => {
-        openHandler()
-        console.log(ownerPercentage, holderPercentage, enableAds);
-        // const contract = await getContract(CMAddresss, CMABI);
-        // console.log(contract);
-        // if (event.target.checked == true) {
-        //   const tx = await contract.publishVideo(data.id);
-        //   await tx.wait();
-        // } else {
-        //   const tx = await contract.unpublishVideo(data.id);
-        //   await tx.wait();
-        // }
-    };
-
-    const UnPublishVideo = (event) => {
-        // use props.data to get all data related to video
+    try {
+      const contract = await getContract(CMAddresss, CMABI);
+      console.log(contract);
+      const tx = await contract.unpublishVideo(props.data.id);
+      await tx.wait();
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return (
-        <div
-            className=" w-[345px] bg-[#2c2c2e] rounded-lg p-4"
-        >
-            <CardMedia
-                sx={{ height: 140 }}
-                image={`https://ipfs.io/ipfs/${props.data.thumbnail}`}
-                title="thumbnail"
+  return (
+    <div className=" w-[345px] bg-[#2c2c2e] rounded-lg p-4">
+      <CardMedia
+        sx={{ height: 140 }}
+        image={`https://ipfs.io/ipfs/${props.data.thumbnail}`}
+        title="thumbnail"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" color="#aa9bd1">
+          {props.data.title}
+        </Typography>
+        <Typography variant="body2" color="#dcd3f2">
+          {props.data.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {props.data.Listed == true ? (
+          <FormControlLabel
+            control={<Android12Switch defaultChecked />}
+            label="UnList From marketplace"
+            color="GrayText"
+            onChange={(event) => ListVideo(event, data)}
+          />
+        ) : (
+          <>
+            {enableVideoListing ? (
+              <>
+                <FormControlLabel
+                  control={<Android12Switch />}
+                  label="List on marketplace"
+                  onChange={(event) => ListVideo(event, data)}
+                  color="GrayText"
+                />
+                <div class="relative mb-3" data-te-input-wrapper-init>
+                  <input
+                    type="number"
+                    defaultValue={props.data.Price}
+                    onChange={(e) => ListingPrice(e, props.data)}
+                    className=" border-b-2 border-gray-500 p-2 bg-transparent w-full "
+                    id="exampleFormControlInput1"
+                    placeholder="listing price"
+                  />
+                  <label for="exampleFormControlInput1" className="">
+                    listing price
+                  </label>
+                </div>
+              </>
+            ) : (
+              <>
+                <FormControlLabel
+                  control={<Android12Switch />}
+                  label="List on marketplace"
+                  color="GrayText"
+                  onChange={(event) => ListVideo(event, props.data)}
+                  disabled
+                />
+                <div class="relative mb-3" data-te-input-wrapper-init>
+                  <input
+                    type="number"
+                    defaultValue={props.data.Price}
+                    onChange={(e) => ListingPrice(e, props.data)}
+                    className="border-b-2 border-gray-500 p-2 bg-transparent w-full "
+                    id="exampleFormControlInput1"
+                    placeholder="listing price"
+                  />
+                  <label for="exampleFormControlInput1" className="">
+                    listing price
+                  </label>
+                </div>
+              </>
+            )}
+          </>
+        )}
+      </CardActions>
+      <CardActions>
+        {props.data.Published == true ? (
+          <FormControlLabel
+            control={<Android12Switch defaultChecked />}
+            label="Unpublish "
+            onChange={(event) => UnPublishVideo(event)}
+          />
+        ) : (
+          <>
+            <FormControlLabel
+              control={<Android12Switch />}
+              label="Publish"
+              onChange={(event) => openHandler()}
             />
-            <CardContent>
-                <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    color="#aa9bd1"
-                >
-                    {props.data.title}
-                </Typography>
-                <Typography variant="body2" color="#dcd3f2">
-                    {props.data.description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                {props.data.Listed == true ? (
-                    <FormControlLabel
-                        control={<Android12Switch defaultChecked />}
-                        label="UnList From marketplace"
-                        color="GrayText"
-                        onChange={(event) => ListVideo(event, data)}
-                    />
-                ) : (
-                    <>
-                        {enableVideoListing ? (
-                            <>
-                                <FormControlLabel
-                                    control={<Android12Switch />}
-                                    label="List on marketplace"
-                                    onChange={(event) => ListVideo(event, data)}
-                                    color="GrayText"
-                                />
-                                <div
-                                    class="relative mb-3"
-                                    data-te-input-wrapper-init
-                                >
-                                    <input
-                                        type="number"
-                                        defaultValue={props.data.Price}
-                                        onChange={(e) => ListingPrice(e, props.data)}
-                                        className=" border-b-2 border-gray-500 p-2 bg-transparent w-full "
-                                        id="exampleFormControlInput1"
-                                        placeholder="listing price"
-                                    />
-                                    <label
-                                        for="exampleFormControlInput1"
-                                        className=""
-                                    >
-                                        listing price
-                                    </label>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <FormControlLabel
-                                    control={<Android12Switch />}
-                                    label="List on marketplace"
-                                    color="GrayText"
-                                    onChange={(event) => ListVideo(event, props.data)}
-                                    disabled
-                                />
-                                <div
-                                    class="relative mb-3"
-                                    data-te-input-wrapper-init
-                                >
-                                    <input
-                                        type="number"
-                                        defaultValue={props.data.Price}
-                                        onChange={(e) => ListingPrice(e, props.data)}
-                                        className="border-b-2 border-gray-500 p-2 bg-transparent w-full "
-                                        id="exampleFormControlInput1"
-                                        placeholder="listing price"
-                                    />
-                                    <label
-                                        for="exampleFormControlInput1"
-                                        className=""
-                                    >
-                                        listing price
-                                    </label>
-                                </div>
-                            </>
-                        )}
-                    </>
-                )}
-            </CardActions>
-            <CardActions>
-                {props.data.Published == true ? (
-                    <FormControlLabel
-                        control={<Android12Switch defaultChecked />}
-                        label="unpublish video"
-                        onChange={(event) => UnPublishVideo(event)}
-                    />
-                ) : (
-                    <>
-                        <FormControlLabel
-                            control={<Android12Switch />}
-                            label="publish"
-                            onChange={(event) => PublishVideo()}
-                        />
-                    </>
-                )}
-            </CardActions>
-            <PublishModal publishVideoHandler={PublishVideo} open={open} openHandler={openHandler} />
-        </div>
-    )
+          </>
+        )}
+      </CardActions>
+      <PublishModal
+        publishVideoHandler={PublishVideo}
+        open={open}
+        openHandler={openHandler}
+      />
+    </div>
+  );
 }
