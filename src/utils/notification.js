2d393d2df15644374af527c64e09bb93bf842f01
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-05-31 02:57:38 +0530

    modified notification button

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -2,6 +2,7 @@ import { ethers } from "ethers";
 import { useAccount } from "wagmi";
 import Button from "@mui/material/Button"
 import * as PushAPI from "@pushprotocol/restapi";
+import { MdNotificationsActive } from "react-icons/md"
 
 const NotificationOptIn = () => {
 
@@ -28,7 +29,9 @@ const NotificationOptIn = () => {
 
     return (
         <>
-            <Button variant="contained" className=" bg-cyan-700" onClick={OptIn}>Opt In</Button>
+            <button type="button" onClick={OptIn} class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+                <MdNotificationsActive /> Enable Notification
+            </button>
         </>
     )
 
@@ -46,24 +49,27 @@ const NotificationOptOut = () => {
         await PushAPI.channels.unsubscribe({
             signer: _signer,
             channelAddress: 'eip155:5:0x2D449c535E4B2e07Bc311fbe1c14bf17fEC16AAb', // channel address in CAIP
-            userAddress:  `eip155:5:${address}`, // user address in CAIP
+            userAddress: `eip155:5:${address}`, // user address in CAIP
             onSuccess: () => {
-             console.log('opt out success');
+                console.log('opt out success');
             },
             onError: () => {
-              console.error('opt out error');
+                console.error('opt out error');
             },
             env: 'staging'
-          })
+        })
     }
 
     return (
         <>
-            <Button variant="contained" className="bg-sky-600 mr-3" onClick={OptOut}>Opt Out</Button>
+            {/* <button type="button"  class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+                <MdNotificationsActive /><span> Disable Notification</span>
+            </button> */}
+            <span className="text-gray-500">want to opt out of notifications? <button onClick={OptOut} className=" hover:underline">click here</button></span>
         </>
     )
 
 }
 
-export { NotificationOptIn, NotificationOptOut,}
+export { NotificationOptIn, NotificationOptOut, }
 
